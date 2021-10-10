fb236a3452a75770f2fd241c28f19e0e9ab183a6
JeffHauckJr <jefferyhauckjr@gmail.com>
2021-10-09 20:33:58 -0500

    basic auto fight sequence done working on site

--- player.js
+++ player.js
@@ -1,115 +1,105 @@
 // turn 1 is player turn 2 is enemy
-let turn = 1;
-
+let turn;
 let player = {};
 let enemy = {};
-
-
-const startFight = () => {
-  const sword = {
-    name: "sword",
-    damage: 20,
-    type: "slicing",
-    hit: 16,
-  };
-
-  player.name = "Chris";
-  player.health = 50;
-  player.equipment = [{
-      name: "Poison Spell",
-      damage: 12
-  }];
-  player.strength = 6;
-  player.ac = 12;
-
-  //create different equipment that add on to stats such as health etc
-
-  enemy.name = "Macho Man Randy Savage";
-  enemy.health = 75;
-  enemy.equipment = [{
-      name: 'Steel Chair',
-      damage: 15
-  }];
-  enemy.strength = 10;
-  enemy.ac = 15;
+let winner;
+player.name = "Derek";
+enemy.name = "Shmergle";
+const sword = {
+  name: "sword",
+  damage: 20,
+  type: "slicing",
+  hit: 16,
 };
 
-//Run through if else statments until a health bar === 0
+const startFight = () => {
+  //player stats
+  player.totalHealth = 50;
+  player.currentHealth = 50;
+  player.equipment = [
+    {
+      name: "Claymore",
+      damage: 20,
+    },
+  ];
+  player.attackBonus = 3;
+  player.ac = 14;
+  //enemy stats
+  enemy.totalHealth = 50;
+  enemy.currentHealth = 50;
+  enemy.equipment = [
+    {
+      name: "Rapier",
+      damage: 10,
+    },
+  ];
+  enemy.attackBonus = 6;
+  enemy.ac = 15;
+};
+//coin flip for first turn
+let coin = Math.floor(Math.random() * 2);
+if (coin === 1) {
+  turn = 1;
+  console.log(`${player.name} goes first`);
+} else {
+  turn = 2;
+  console.log(`${enemy.name} goes first`);
+}
 
-// let timerId = setInterval(() => console.log('tick'), 2000);
-
-// // after 5 seconds stop
-// setTimeout(() => { clearInterval(timerId); console.log('stop'); }, 5000);
-
-// const function1 = () => {
-//     console.log("Function 1")
-// }
-
-// const function2 = () => {
-//     console.log("Function 2")
-// }
-
-// setTimeout(function() {
-//     function1() // runs first
-
-//     // runs second
-//   }, 1000)
-
-// const object1 = { property1: "Value 1" };
-// const object2 = { property2: "Value 2" };
-
-// const listOfObjects = [object1, object2];
-// const copyOfListOfObjects = [...listOfObjects];
-
-// function updateObjects(objects) {
-//   return objects.map((object) => {
-//     Object.keys(object).forEach((property) => {
-//       object[property] = "Updated value";
-//     });
-
-//     return object;
-//   });
-// }
-
-// const listOfUpdatedObjects = updateObjects(listOfObjects);
-
-// console.log(listOfObjects);
-// console.log(copyOfListOfObjects);
-// console.log(listOfUpdatedObjects);
-
+//combat sequence
 const simpleCombat = () => {
   let combatInterval = setInterval(function () {
+    //if players turn
     if (turn === 1) {
-      let acRoll = Math.floor(Math.random() * 20) + player.strength;
+      //rolling to hit enemy
+      let acRoll = Math.floor(Math.random() * 20) + player.attackBonus;
       console.log(`${player.name} attacks with a ${player.equipment[0].name}`);
-      if (player.health > 0) {
+      //checking if player is alive
+      if (player.currentHealth > 0) {
+        //checking if you hit enemy ac
         if (acRoll >= enemy.ac) {
-          if (player.equipment[0].damage >= enemy.health) {
+          //checking if attack kills declares winner
+          if (player.equipment[0].damage >= enemy.currentHealth) {
             console.log(`${player.name} has slain ${enemy.name}`);
+            winner = player.name;
+            simpleAfterCombat();
             clearInterval(combatInterval);
+            //taking damage from attack changing turns
           } else {
-            enemy.health = enemy.health - player.equipment[0].damage;
+            enemy.currentHealth =
+              enemy.currentHealth - player.equipment[0].damage;
             turn = 2;
-            console.log(`${enemy.name} is looking hurt`, enemy.health);
+            console.log(`${enemy.name} was hit`, enemy.currentHealth);
           }
+          //if attack roll misses changing turns
         } else {
           console.log(`${enemy.name} has dodged ${player.name}'s attack`);
           turn = 2;
         }
       }
+      //if enemy turn
     } else {
-      if (enemy.health > 0) {
-        let acRoll = Math.floor(Math.random() * 20);
+      //checking if enemy is alive
+      if (enemy.currentHealth > 0) {
+        let acRoll = Math.floor(Math.random() * 20) + enemy.attackBonus;
+        //rolling for attack
         console.log(`${enemy.name} attacks with a ${enemy.equipment[0].name}`);
+        //if attack hits plaayer
         if (acRoll >= player.ac) {
-          if (enemy.equipment[0].damage >= player.health) {
+          //checking if attack kills declares winner
+          if (enemy.equipment[0].damage >= player.currentHealth) {
             console.log(`${enemy.name} has slain ${player.name}`);
+            winner = enemy.name;
+            simpleAfterCombat();
             clearInterval(combatInterval);
+            //taking damage from attack and changing turns
           } else {
-            player.health = player.health - enemy.equipment[0].damage;
+            player.currentHealth =
+              player.currentHealth - enemy.equipment[0].damage;
             turn = 1;
-            console.log(`${player.name} is looking hurt`, player.health);
+            console.log(`${player.name} was hit`, player.currentHealth);
           }
+          //if attack misses and turn changes
         } else {
           console.log(`${player.name} has dodged ${enemy.name}'s attack`);
           turn = 1;
@@ -119,6 +109,17 @@ const simpleCombat = () => {
   }, 3000);
 
   return combatInterval;
+};
+
+const simpleAfterCombat = () => {
+  //checking if player won
+  if (winner === player.name) {
+    console.log(`${player.name} has gained some experience`);
+    console.log(`${enemy.name} has dropped their ${enemy.equipment[0].name}`)
+    //if player lost
+  } else if (winner === enemy.name) {
+    console.log(`You have been defeated`);
+  }
 };
 
 startFight();
